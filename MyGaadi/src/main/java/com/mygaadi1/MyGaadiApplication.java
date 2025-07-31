e490e6357d5a0f60f518515fe1464bc507721498
sahil1504001 <sahilshikalgar001@gmail.com>
2025-07-31 16:25:29 +0530

    Backend blank folder added

---
+++ MyGaadi/src/main/java/com/mygaadi1/MyGaadiApplication.java
@@ -0,0 +1,13 @@
+package com.mygaadi1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyGaadiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyGaadiApplication.class, args);
+	}
+
+}
