7b61619a3efdb8be26328447e99bbb9292a488ca
89333-Samrat <142326294+89333-Samrat@users.noreply.github.com>
2025-07-31 15:41:56 +0530

    Delete Back-end/mygaadi directory
    deleted these file

--- Back-end/mygaadi/src/main/java/com/mygaadi/custom_exceptions/AuthenticationFailureException.java
+++
@@ -1,7 +0,0 @@
-package com.mygaadi.custom_exceptions;
-
-public class AuthenticationFailureException extends RuntimeException {
-    public AuthenticationFailureException(String mesg) {
-        super(mesg);
-    }
-}
