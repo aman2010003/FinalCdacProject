92080e207ebf41ffb5000e2cafc9911c93399964
89333-Samrat <samratmali6569@gmail.com>
2025-08-04 16:56:37 +0530

    car and favorite entity class added

---
+++ MyGaadi/src/main/java/com/mygaadi/entities/Car.java
@@ -0,0 +1,61 @@
+package com.mygaadi.entities;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "cars")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class Car {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long carId;
+
+    @ManyToOne
+    @JoinColumn(name = "seller_id", nullable = false)
+    private User seller;
+
+    private String brand;
+    private String model;
+    private String variant;
+    private int registrationYear;
+
+    @Enumerated(EnumType.STRING)
+    private Ownership ownership;
+
+    private int kmDriven;
+    private String location;
+    private BigDecimal price;
+    private String registrationNumber;
+    private String color;
+    private boolean insuranceValid;
+    private String fuelType;
+    private String transmission;
+    private String description;
+
+    private LocalDateTime createdAt;
+    
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "car",fetch = FetchType.EAGER)
+    private List<Image> list = new ArrayList<>();
+    
+    
+    public List<Image> getImages() {
+        return list;
+    }
+
+    public void addImage(Image image)
+    {
+    	this.list.add(image);
+    	image.setCar(this);
+    }
+}
